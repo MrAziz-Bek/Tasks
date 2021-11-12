b3379af18df96cc3b84d11e4748c2b042f310d0f
Azizbek Boboyev <azizbekboboyev@Azizbeks-MacBook-Pro.local>
2021-11-12 16:03:22 +0500

    Create POST

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,12 +1,29 @@
-namespace Tasks.Controllers;
+using System.Net;
+namespace Tasks.Controller;
 [ApiController]
 [Route("[controller]")]
-public class TaskController
+public class TaskController : ControllerBase
 {
     private readonly ILogger<TaskController> _logger;
+    private readonly IStorageService _storage;
 
-    public TaskController(ILogger<TaskController> logger)
+    public TaskController(ILogger<TaskController> logger, IStorageService storage)
     {
         _logger = logger;
+        _storage = storage;
+    }
+
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public async Task<IActionResult> CreateTask([FromBody] NewTask newTask)
+    {
+        var taskEntity = newTask.ToTaskEntity();
+        var insertResult = await _storage.InsertTaskAsync(taskEntity);
+
+        if (insertResult.IsSuccess)
+        {
+            return CreatedAtAction("Create Task", taskEntity);
+        }
+        return StatusCode((int)HttpStatusCode.InternalServerError, new { message = insertResult.exception.Message });
     }
 }
